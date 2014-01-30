dace744d5bdcad09330c38ee3db929d68bbc9859
Edward Paget <ed.paget@gmail.com>
2014-01-30 15:45:42 -0600

    Fix init script typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,5 +7,4 @@ default['unicorn']['site']['workers'] = 4
 default['unicorn']['site']['timeout'] = 10
 default['unicorn']['site']['max_memory'] = 1400
 default['unicorn']['site']['config_path'] = "/usr/local/unicorn/#{ node['unicorn']['site']['name'] }.rb"
-default['unicorn']['site']['config'] = 'default'
 default['unicorn']['site']['app_path'] = "/rails/apps/#{ node['unicorn']['site']['name'] }/current"
